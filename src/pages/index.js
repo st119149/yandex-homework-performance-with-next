873f1fad74c2ccab7d1ed79babf04353616edd24
Yaroslav <yaro.polyakov@gmail.com>
2024-07-11 21:30:08 +0300

    add header

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,38 +10,37 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Яндекс.Дом</title>
       </Head>
-      <main>
-        <div id="app">
-          <Main />
-        </div>
+      <div id="app">
+        <Header />
+        <Main />
+      </div>
 
-        <footer class="footer">
-          <ul class="footer__list">
-            <li class="footer__item">
-              <a class="footer__link" href="/">
-                Помощь
-              </a>
-            </li>
-            <li class="footer__item">
-              <a class="footer__link" href="/">
-                Обратная связь
-              </a>
-            </li>
-            <li class="footer__item">
-              <a class="footer__link" href="/">
-                Разработчикам
-              </a>
-            </li>
-            <li class="footer__item">
-              <a class="footer__link" href="/">
-                Условия использования
-              </a>
-            </li>
-          </ul>
+      <footer class="footer">
+        <ul class="footer__list">
+          <li class="footer__item">
+            <a class="footer__link" href="/">
+              Помощь
+            </a>
+          </li>
+          <li class="footer__item">
+            <a class="footer__link" href="/">
+              Обратная связь
+            </a>
+          </li>
+          <li class="footer__item">
+            <a class="footer__link" href="/">
+              Разработчикам
+            </a>
+          </li>
+          <li class="footer__item">
+            <a class="footer__link" href="/">
+              Условия использования
+            </a>
+          </li>
+        </ul>
 
-          <div class="footer__copyright">© 1997–2023 ООО «Яндекс»</div>
-        </footer>
-      </main>
+        <div class="footer__copyright">© 1997–2023 ООО «Яндекс»</div>
+      </footer>
     </>
   );
 }
